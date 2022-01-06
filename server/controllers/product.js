4e8f4a79880bdc78824c7e3ac639702283353104
mustafa <mustafamjaber@yahoo.com>
2022-01-06 12:38:26 +0200

    fixed the hero photo and added the search functionalty on the api

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -5,7 +5,21 @@ const product = require('../models/product');
 //* all product route functions
 
 const getAllProducts =async (req,res)=>{
-    const products = await product.find()
+    const {company,name,price,rating,}=req.query;
+    let queryObject = {}
+
+    //*Search by the company
+    if(company){
+        queryObject.company = company
+    }
+
+//*Search by the name
+
+if(name){
+    queryObject.name = {$regex:name,$options:'i'}
+}
+
+    const products = await product.find(queryObject)
     res.status(200).json({products,nbHits:products.length})
 }
 
