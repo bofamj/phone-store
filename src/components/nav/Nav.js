4215774fc605a2c3eb864a2bc238d94a0a2bf7db
mustafa <mustafamjaber@yahoo.com>
2022-01-08 16:09:06 +0200

    instaling react icon added the ctyle to the card added th cart icon to the nav

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { FiMenu } from 'react-icons/fi';
 import {useGlobalContext} from '../../context';
+import { BsCart4 } from 'react-icons/bs';
+
 import './nav.css';
 const Nav = () => {
     const{isOpen,setIsOpen}=useGlobalContext()
@@ -12,6 +14,7 @@ const Nav = () => {
                 <a href="#">Home</a>
                 <a href="#">Project</a>
                 <a href="#">AbotUs</a>
+                <a href="#"><BsCart4/></a>
                 {!isOpen?<FiMenu className="menu" onClick={()=>setIsOpen(!isOpen)}/>:''}
             </nav>
         </header>
--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import './product.css'
+ //MdOutlineAddShoppingCart
+
+
+
+ 
 const Product = ({name,company,imageURL,price,rating}) => {
     return (
         <div className='cart'>
-            <img src={imageURL} alt={name} />
-            {/* <div>
+            <div className='img-continert'>
+                <img src={imageURL} alt={name} />
+            </div>
+             <div className='text-continer'>
                 <h2>{name}</h2>
-                <h3>{price}</h3>
-            </div> */}
+                <h3>${price}</h3>
+            </div> 
         </div>
     )
 }
